1a930de84de373f4310ea86e306617b31371b0e3
lanzhiheng <lanzhihengrj@gmail.com>
2019-12-12 20:51:00 +0800

    feat: Add `this` token.

--- blex.c
+++ blex.c
@@ -8,7 +8,7 @@ const char *const beanX_tokens [] = {
     "+", "-", "*", "/",
     ":", ",", ";",
     "{", "}", "(", ")", "[", "]", ".",
-    "false", "for", "fn", "if",
+    "false", "for", "fn", "if", "this",
     "in", "var", "nil", "not", "or",
     "return", "true",  "while",
     "==", "=", ">=", ">", "<=", "<", "~=",
