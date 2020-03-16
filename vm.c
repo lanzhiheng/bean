018b410332cc2c54e99657d7a9ecf93109ccc5de
lanzhiheng <lanzhihengrj@gmail.com>
2020-03-16 23:34:57 +0800

    feat: Add some basic handler.

--- vm.c
+++ vm.c
@@ -1,13 +1,25 @@
 #include "vm.h"
 #include "stdio.h"
+#include "bstring.h"
 #include "bstate.h"
 
+typedef struct Thread {
+  TValue ** stack;
+  TValue ** esp;
+} Thread;
+
 int executeInstruct(bean_State * B) {
+  Thread * thread = malloc(sizeof(Thread));
+  thread -> stack = malloc(sizeof(TValue*) * 3000);
+  thread -> esp = thread -> stack;
+
   char * ip;
   ip = G(B)->instructionStream->buffer;
   char code;
 
-#define DECODE loopStart: \
+#define PUSH(value) (*thread->esp++ = value)
+#define POP(value) (*(--thread->esp))
+#define DECODE loopStart:                       \
   code = *ip++; \
   switch (code)
 #define CASE(shortOp) case OP_BEAN_##shortOp
@@ -29,11 +41,22 @@ int executeInstruct(bean_State * B) {
       LOOP();
     }
     CASE(PUSH_TRUE): {
-      printf("true\n");
+      PUSH(G(B)->tVal);
       LOOP();
     }
     CASE(PUSH_FALSE): {
-      printf("false\n");
+      PUSH(G(B)->fVal);
+      LOOP();
+    }
+    CASE(PUSH_NIL): {
+      PUSH(G(B)->nil);
+      LOOP();
+    }
+    CASE(INSPECT): {
+      TValue * value = POP();
+      TValue * tvString = tvalue_inspect(B, value);
+      char * string = getstr(svalue(tvString));
+      printf("%s\n", string);
       LOOP();
     }
     default: {
