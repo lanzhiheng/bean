13df1e068116eeaddc07cce264847fdddc33a51a
lanzhiheng <lanzhihengrj@gmail.com>
2020-03-29 14:14:42 +0800

    feat: Remove the http module temporarily.

--- test.rb
+++ test.rb
@@ -3,6 +3,8 @@ array = []
 Dir.glob('tests/*').each do |filename|
   result = `./bean #{filename}`
 
+  next if (filename.end_with?("http.bn"))
+
   status = ($?).exitstatus
   if (status != 0)
     print "\u001b[31mF\u001b[0m"
